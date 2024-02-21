b94cee2a5a8c550ded997c628ce41b16bc4b86af
Giriiri-g <giri090205@gmail.com>
2024-02-22 01:43:15 +0530

    Bomb Placing Stable

--- Rogue Bomber/game.py
+++ Rogue Bomber/game.py
@@ -1,14 +1,16 @@
 ##[ MAZE-bOMB ]
-
+import  random
 import pygame
 import sys
 
 def run_game(screen):
-    global Map, Player_pos, playermap, lines, menu_active, hud_surface, hud_names, hud_values, font2, player_huds, font
+    global Map, Player_pos, playermap, lines, menu_active, hud_surface, hud_names, hud_values, font2, player_huds, font, bombs, players
 
+
+    players = [[10,9], [70,5]]
     white = (255, 255, 255)
     black = (0, 0, 0)
-    blue = (0, 255, 0)
+    red = (255, 0, 0)
     grey = (169,169,169)
     wall_color = (170, 85, 0)
     movable_space_color = (80, 240, 80)
@@ -20,69 +22,64 @@ def run_game(screen):
     hud_surface = pygame.Surface((1200, 60))
     hud_surface.fill((169, 169, 169))
     hud_names = ["Giriirig", "PsylectrA", "Puchandi", "cumlord"]
-    hud_values = [["♥♥♥♥♥", 1, 1], ["♥♥♥", 0, 2], ["", 3, 0], ["♥♥♥♥", 2, 1]]
+    hud_values = [["♥♥♥♥♥", 0, 1], ["♥♥♥", 0, 2], ["", 3, 0], ["♥♥♥♥", 2, 1]]
     surface_width = 298
     surface_height = 58
     player_huds = [pygame.Surface((surface_width, surface_height)) for _ in range(4)]
+    bombs = []
 
     pygame.init()
-    # screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
     pygame.display.set_caption("Rogue Bomber")
 
     Map =  """       ╔══════════╗                                                   
        ║∙∙∙∙∙∙∙∙∙∙║                               ╔══════════════════╗
-       ║∙∙∙∙∙∙∙∙∙∙║                               ║∙∙∙∙∙∙∙∙!∙∙∙∙∙∙∙∙∙║
-       ╚══════╦═══╝                            ▒▒▒╣∙∙∙∙∙∙∙∙∙∙∙∙∙!∙∙∙∙║
+       ║∙∙∙∙∙∙∙∙∙∙║                               ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
+       ╚══════╦═══╝                            ▒▒▒╣∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
               ▒                         ▒▒▒▒▒▒▒▒  ╚╦═════════════════╝
               ▒                         ▒          ▒                  
         ▒▒▒▒▒▒▒                    ▒▒▒▒▒▒          ▒▒▒▒▒▒             
 ╔═══════╩══════════╗               ▒              ╔═════╩════════════╗
 ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙╠▒▒▒            ▒              ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
 ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║  ▒  ╔═════════╩═╗            ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
-║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║  ▒▒▒╣∙∙∙∙∙∙∙∙∙!∙║            ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
-║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║     ║∙∙∙∙∙∙∙∙∙∙∙╠▒▒▒▒▒▒▒▒▒▒▒▒╣∙∙∙∙∙∙∙∙∙∙∙∙!∙∙∙∙∙║
-║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║     ║∙∙∙∙!∙∙∙∙∙∙║            ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
+║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║  ▒▒▒╣∙∙∙∙∙∙∙∙∙∙∙║            ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
+║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║     ║∙∙∙∙∙∙∙∙∙∙∙╠▒▒▒▒▒▒▒▒▒▒▒▒╣∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
+║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║     ║∙∙∙∙∙∙∙∙∙∙∙║            ║∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙∙║
 ╚═══════╦══════════╝     ╚═════════╦═╝            ╚══════════════════╝
         ▒                   ▒▒▒▒▒▒▒▒                                  
         ▒▒▒▒▒▒▒▒▒▒▒       ╔═╩═╗                                       
        ╔══════════╩═╗    ▒╣∙∙∙║                          ╔════╗       
        ║∙∙∙∙∙∙∙∙∙∙∙∙╠▒▒▒▒▒║∙∙∙║                          ║∙∙∙∙║       
-       ║∙∙∙∙∙∙∙∙∙∙∙∙║     ║∙∙∙║                          ║∙!∙∙║       
+       ║∙∙∙∙∙∙∙∙∙∙∙∙║     ║∙∙∙║                          ║∙∙∙∙║       
        ║∙∙∙∙∙∙∙∙∙∙∙∙║     ║∙∙∙║  ▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒▒╣∙∙∙∙║       
-       ║∙∙∙∙∙!∙∙∙∙∙∙║     ║∙∙∙╠▒▒▒                       ║∙∙∙∙║       
+       ║∙∙∙∙∙∙∙∙∙∙∙∙║     ║∙∙∙╠▒▒▒                       ║∙∙∙∙║       
        ╚════════════╝     ╚═══╝                          ╚════╝       
 """
 
 
     def move(d):
-        global Player_pos
-        if d=="a":
-            calcpos = [Player_pos[0], Player_pos[1]-1]
-            calcmappos = mapindexer(calcpos)
-            if checkvalid(calcmappos):
-                Player_pos = calcpos
-                drawplayer(calcmappos)
-                
-        elif d=="s":
-            calcpos = [Player_pos[0]+1, Player_pos[1]]
-            calcmappos = mapindexer(calcpos)
-            if checkvalid(calcmappos):
-                Player_pos = calcpos
-                drawplayer(calcmappos)
-        elif d=="w":
-            calcpos = [Player_pos[0]-1, Player_pos[1]]
-            calcmappos = mapindexer(calcpos)
-            if checkvalid(calcmappos):
-                Player_pos = calcpos
-                drawplayer(calcmappos)
-        elif d=="d":
-            calcpos = [Player_pos[0], Player_pos[1]+1]
-            calcmappos = mapindexer(calcpos)
-            if checkvalid(calcmappos):
-                Player_pos = calcpos
-                drawplayer(calcmappos)
+        global Player_pos, lines, Map, hud_values
+        new_pos = Player_pos[:]  # Create a copy of the player's position
+        if d == "a":
+            new_pos[1] -= 1
+        elif d == "s":
+            new_pos[0] += 1
+        elif d == "w":
+            new_pos[0] -= 1
+        elif d == "d":
+            new_pos[1] += 1
+
+        new_map_pos = mapindexer(new_pos)
+        if checkvalid(new_map_pos):
+            if Map[new_map_pos] == '!':
+                Map = Map[:new_map_pos] + '∙' + Map[new_map_pos + 1:]
+                if hud_values[0][1] < 3:
+                    hud_values[0][1]+=1
+            Player_pos = new_pos
+            drawplayer(new_map_pos)
+            lines = playermap.split('\n')
 
     def checkvalid(mappos):
+        global Map
         if Map[mappos] in ['║','═','╗','╝','╚','╔', " "]:
             return False
         return True
@@ -91,7 +88,7 @@ def run_game(screen):
         return pos[0]*71 + pos[1] + 1
 
     def drawplayer(mappos):
-        global playermap
+        global playermap, Map
         playermap = Map[:mappos]+"@"+Map[mappos+1:]
 
     def draw_map():
@@ -100,23 +97,19 @@ def run_game(screen):
             for j, char in enumerate(line):
                 color = black  # Default color for characters not explicitly defined
                 if char in ['║','═','╗','╝','╚','╔','╦','╣','╠','╩']:
-                        color = wall_color
+                    color = wall_color
                 elif char == "∙":
-                        color = movable_space_color
-                elif char == "!":
-                        color = exclamation_color
+                    color = movable_space_color
+                elif char in ["!", "☼"]:
+                    color = exclamation_color
                 elif char == "▒":
-                        color = passage_color
+                    color = passage_color
                 elif char == "@":
-                        color = player_color
+                    color = player_color
 
                 text_surface = font.render(char, True, color)
                 offset = (j * 15 + 75, i * 28 + 120)
                 screen.blit(text_surface, offset)
-                # text_rect = text_surface.get_rect()
-                # text_rect.topleft = offset
-                # screen.blit(text_surface, text_rect)
-                # pygame.draw.rect(screen, (255, 255, 255), text_rect, 1)
 
     def draw_hud():
         hud_surface.fill(grey)
@@ -124,19 +117,26 @@ def run_game(screen):
         y_position = 1
         screen.blit(hud_surface, (0, 0))
         for i, (name, values) in enumerate(zip(hud_names, hud_values)):
+            if len(values[0]) == 0:
+                font_color = red
+            else:
+                font_color = white
             player_huds[i].fill((0, 0, 0))
-            name_text = font2.render(name, True, white)
+            name_text = font2.render(name, True, font_color)
             player_huds[i].blit(name_text, (10, 5))
-            hearts_text = font2.render(values[0], True, white)
+            hearts_text = font2.render(values[0], True, font_color)
             player_huds[i].blit(hearts_text, (200, 6))
-            val1_text = font2.render("☼x"+str(values[1]), True, white)
+            val1_text = font2.render("☼x"+str(values[1]), True, font_color)
             player_huds[i].blit(val1_text, (10, 30))
-            val2_text = font2.render("ɸx"+str(values[2]), True, white)
+            val2_text = font2.render("ɸx"+str(values[2]), True, font_color)
             player_huds[i].blit(val2_text, (val1_text.get_width() + 200, 30))
             screen.blit(player_huds[i], (x_positions[i], y_position))
 
 
-
+    def place_bomb():
+         global Player_pos, Map
+         Map = Map[:mapindexer(Player_pos)] + '☼' + Map[mapindexer(Player_pos)+1:]
+         drawplayer(mapindexer(Player_pos))
 
     Player_pos = [10, 9]
     Map_pos = 720
@@ -166,7 +166,7 @@ def run_game(screen):
     menu_lines = desc.split('\n')
     menu_height = len(menu_lines) * font.get_height()
     menu = pygame.Surface((font.size(max(menu_lines, key=len))[0], menu_height), pygame.SRCALPHA)
-    menu.fill((0, 0, 0, 0))  # Make the surface transparent
+    menu.fill((0, 0, 0, 0))
 
     for i, line in enumerate(menu_lines):
         text_surface = font.render(line, True, grey)
@@ -192,16 +192,16 @@ def run_game(screen):
                         sys.exit()
                 if event.key == pygame.K_a and menu_active == False:
                         move("a")
-                        lines = playermap.split('\n')
                 if event.key == pygame.K_s and menu_active == False:
                         move("s")
-                        lines = playermap.split('\n')
                 if event.key == pygame.K_w and menu_active == False:
                         move("w")
-                        lines = playermap.split('\n')
                 if event.key == pygame.K_d and menu_active == False:
                         move("d")
-                        lines = playermap.split('\n')
+                if event.key == pygame.K_b and menu_active == False:
+                        if hud_values[0][1]>0:
+                            place_bomb()
+                            hud_values[0][1] -= 1
 
                 if (event.key == pygame.K_ESCAPE):
                         menu_active = False
@@ -214,10 +214,14 @@ def run_game(screen):
                         
                         
                         
-
+        if Map.count('!') <= 8 and random.random() < 0.005:
+            movable_indices = [i for i, char in enumerate(Map) if char == '∙']
+            if movable_indices:
+                random_index = random.choice(movable_indices)
+                Map = Map[:random_index] + '!' + Map[random_index + 1:]
 
         
-        # displaying each line in a text surface for efficient manipulation 
+
         if menu_active == False:
             draw_map()
             draw_hud()
@@ -225,9 +229,9 @@ def run_game(screen):
         clock.tick(60)
 
 
-# import assets
-# pygame.init()
-# asset = assets.Asset()
-# screen = pygame.display.set_mode((asset.width, asset.height))
-# pygame.display.set_caption("Rogue Bomber")
-# run_game(screen)
+import assets
+pygame.init()
+asset = assets.Asset()
+screen = pygame.display.set_mode((asset.width, asset.height))
+pygame.display.set_caption("Rogue Bomber")
+run_game(screen)
